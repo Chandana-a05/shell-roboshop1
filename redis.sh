69028a869c99b25047fe05d82d4dd00d9efd49ae
chandana <allu.chandu05@gmail.com>
2026-02-01 09:13:52 -0600

    actions

---
+++ redis.sh
@@ -0,0 +1,41 @@
+#!/bin/bsh
+
+LOGS_FOLDER="/var/log/shell-roboshop1"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m" #Normal
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.devopspro.online
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi  
+
+mkdir -p $LOGS_FOLDER
+echo "Script start executed at : $(date)" | tee -a $LOGS_FILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ......FAILURE" | tee -a $LOGS_FILE
+        exit1
+    else
+         echo "$2..... SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+
+dnf module disable redis -y
+dnf module enable redis:7 -y
+dnf install redis -y 
+VALIDATE $? "Installing Redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl enable redis 
+systemctl start redis 
+VALIDATE $? "Starting redis"
