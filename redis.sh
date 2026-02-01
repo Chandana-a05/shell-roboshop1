2c941901af52d7526ba60918e3aa7a540fc85bb6
chandana <allu.chandu05@gmail.com>
2026-02-01 10:35:38 -0600

    actions

--- redis.sh
+++ redis.sh
@@ -29,21 +29,15 @@ VALIDATE(){
 }
 
 
-dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disabling Default Redis"
-dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Redis 7"
-dnf install redis -y  &>>$LOG_FILE
+dnf module disable redis -y
+dnf module enable redis:7 -y
+dnf install redis -y &>>$LOGS_FILE
 VALIDATE $? "Installing Redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Allowing Remote connections to Redis"
 
-systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "Enabling Redis"
-systemctl start redis &>>$LOG_FILE
+sed -i 's/127.0.0.1/0.0.0.0/g' -i 's/protected-mode yes/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing Redis remote connections"
+
+systemctl enable redis 
+systemctl start redis 
 VALIDATE $? "Starting Redis"
-
-
-
-
